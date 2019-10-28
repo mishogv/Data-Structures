20502351083075833b2b3258a38fe50040c9c795
Mihail <mihogv@gmail.com>
2019-10-28 21:52:27 +0200

    Downloaded Binary Search Tree skeleton from SoftUni

---
+++ src/Binary Search Tree/BinarySearchTree/Launcher.cs
@@ -0,0 +1,26 @@
+﻿namespace Launcher
+{
+    using System;
+
+    public class Launcher
+    {
+        public static void Main(string[] args)
+        {
+            BinarySearchTree<int> bst = new BinarySearchTree<int>();
+
+            bst.Insert(10);
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(1);
+            bst.Insert(4);
+            bst.Insert(8);
+            bst.Insert(9);
+            bst.Insert(37);
+            bst.Insert(39);
+            bst.Insert(45);
+
+            bst.EachInOrder(Console.WriteLine);
+
+        }
+    }
+}
